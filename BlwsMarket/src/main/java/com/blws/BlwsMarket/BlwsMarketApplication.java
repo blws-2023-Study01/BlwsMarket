ee154fd6138c90ec9a334c0a3bfeba69f611a880
Swan <swan@blws.co.kr>
2023-01-25 09:13:28 +0900

    Feat : 프로젝트초기화

---
+++ BlwsMarket/src/main/java/com/blws/BlwsMarket/BlwsMarketApplication.java
@@ -0,0 +1,13 @@
+package com.blws.BlwsMarket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlwsMarketApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BlwsMarketApplication.class, args);
+	}
+
+}
---
+++ BlwsMarket/src/test/java/com/blws/BlwsMarket/BlwsMarketApplicationTests.java
@@ -0,0 +1,13 @@
+package com.blws.BlwsMarket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlwsMarketApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
